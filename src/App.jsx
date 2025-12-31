1df349fa15f5c7a1eeaaccb09b845738afac4287
icy-s <evgeny.tailov@gmail.com>
2025-12-31 16:37:58 +0200

    init commit (components, props, styles, states concepts)

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({ title }) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(true)}>
+        Like
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+  // 1 arg - var name, 2 arg - set var name
+  const [hasLiked, setHasLiked] = useState(false);
+
+  return (
+    <div className="card-container">
+      <Card title="Star Wars"/>
+      <Card title="Avatar"/>
+      <Card title="The Lion King"/>
+    </div>
+  )
+}
+
+export default App
