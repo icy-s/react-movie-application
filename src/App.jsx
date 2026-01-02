fb26301cdef456f0e48011057acb16abee0052fa
icy-s <evgeny.tailov@gmail.com>
2026-01-02 21:18:31 +0200

    API fetching setup, movie card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,59 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import Spinner from "./components/Spinner"
 import Search from "./components/Search"
+import MovieCard from "./components/MovieCard"
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        authorization: `Bearer ${API_KEY}` 
+    }
+}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState('')
+    const [movieList, setMovieList] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    
+    const fetchMovies = async () => {
+        setIsLoading(true)
+        setErrorMessage('')
+
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+            const response = await fetch(endpoint, API_OPTIONS)
+        
+            if(!response.ok) {
+                throw new Error('Failed to fetch movies')
+            }
+
+            const data = await response.json()
+
+            if(data.response === 'False') {
+                setErrorMessage(data.Error || 'Failed to fetch movies')
+                setMovieList([])
+                return
+            }
+
+            setMovieList(data.results || [])
+        } catch (error) {
+            console.error(`Error fetching mvoies: ${error}`)
+            setErrorMessage('Error fetching movies. Please try again later.')
+        } finally {
+          setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
 
     return (
         <main>
@@ -15,6 +66,24 @@ const App = () => {
                 </header>
 
                 <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <h1 className="text-white">{searchTerm}</h1>
+            
+                <section className="all-movies">
+                    <h2 className="mt-[40px]">All Movies</h2>
+                
+                    {isLoading ? (
+                        <Spinner />
+                    ) : errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                // mapping with key is required for precision when deleting, for example, one specific movie from list
+                                <MovieCard key={movie.id} movie={movie}/>
+                            ))}
+                        </ul>
+                    )}
+                </section>
             </div>
         </main>
     )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,18 @@
 // destructurizing component, so there will be no need to type props.searchTerm every time
-const Search = (searchTerm, setSearchTerm) => {
+const Search = ({searchTerm, setSearchTerm}) => {
     return (
-        <div className="text-white text-3xl">{searchTerm}</div>
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" />
+            
+                <input
+                    type="text"
+                    placeholder="Search through thousands of movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
     )
 }
 
