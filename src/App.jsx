51d10acadd63101c95167f75734cd4134e19233a
icy-s <evgeny.tailov@gmail.com>
2026-01-03 18:49:54 +0200

    search + debouncing optimization, trending movies code + design, deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import Spinner from "./components/Spinner"
 import Search from "./components/Search"
 import MovieCard from "./components/MovieCard"
+import { useDebounce } from "react-use"
+import { getTrendingMovies, updateSearchCount } from "./appwrite"
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 
@@ -16,17 +18,28 @@ const API_OPTIONS = {
 }
 
 const App = () => {
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
     const [searchTerm, setSearchTerm] = useState('');
-    const [errorMessage, setErrorMessage] = useState('')
+    
     const [movieList, setMovieList] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
     const [isLoading, setIsLoading] = useState(false)
     
-    const fetchMovies = async () => {
+    const [trendingMovies, setTrendingMovies] = useState('')
+
+    // debounce is used to prevent making too many API requests at short amount of time
+    // waiting specified time after user stopped typing in ms and then sends API request of user's search term
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+    const fetchMovies = async (query = '') => {
         setIsLoading(true)
         setErrorMessage('')
 
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            const endpoint = query
+            // encodeUriComponent is used for correctly fetching API request, no matter the characters in query
+            ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
 
             const response = await fetch(endpoint, API_OPTIONS)
         
@@ -43,16 +56,37 @@ const App = () => {
             }
 
             setMovieList(data.results || [])
+
+            if(query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0])
+            }
         } catch (error) {
-            console.error(`Error fetching mvoies: ${error}`)
+            console.error(`Error fetching movies: ${error}`)
             setErrorMessage('Error fetching movies. Please try again later.')
         } finally {
           setIsLoading(false)
         }
     }
 
+    const loadTrendingMovies = async () => {
+        try {
+          const movies = await getTrendingMovies()
+
+          setTrendingMovies(movies)
+        } catch (error) {
+          console.error(`Error fetching trending movies: ${error}`)
+        }
+    }
+
+    // when searched movie query changes, it is fetched through the deps array
     useEffect(() => {
-        fetchMovies()
+        fetchMovies(debouncedSearchTerm)
+    }, [debouncedSearchTerm])
+
+    // using separate useEffect for loading trending movies to not update trending movies every time user searches for something,
+    // also leaving dependencies array empty
+    useEffect(() => {
+        loadTrendingMovies()
     }, [])
 
     return (
@@ -63,13 +97,27 @@ const App = () => {
                 <header>
                     <img src="./hero.png" alt="Hero Banner"/>
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-                </header>
 
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-                <h1 className="text-white">{searchTerm}</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                </header>
             
+                {trendingMovies.length > 0 && (
+                    <section className="trending">
+                        <h2>Trending Movies</h2>
+
+                        <ul>
+                            {trendingMovies.map((movie, index) => (
+                                <li key={movie.$id}>
+                                    <p>{index + 1}</p>
+                                    <img src={movie.poster_url} alt={movie} />
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                )}
+
                 <section className="all-movies">
-                    <h2 className="mt-[40px]">All Movies</h2>
+                    <h2>All Movies</h2>
                 
                     {isLoading ? (
                         <Spinner />
